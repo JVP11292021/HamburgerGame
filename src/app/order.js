5efe5af3fc2c6ee6c4d705e1e4e4983f3915ae8a
JVP11292021 <jessyvanpolanen@gmail.com>
2024-01-22 11:27:59 +0100

    Dynamic asset loading

--- src/app/order.js
+++ src/app/order.js
@@ -1,9 +1,53 @@
-const OrderList = {
-    burgers: [],
-    lettuce: [],
-    tomato: [],
-    burgerTop: [],
-    burgerBottom: []
+const BURGER_CLASS = ".burger";
+const LETTUCE_CLASS = ".lettuce";
+const TOMATO_CLASS = ".tomato";
+const BURGER_TOP_CLASS = ".burger.top";
+const BURGER_BOTTOM_CLASS = ".burger.bottom";
+
+class Order {
+    burgers;
+    lettuce;
+    tomato;
+    burgerTop;
+    burgerBottom;
+
+    constructor(
+        burgers,
+        lettuce,
+        tomato,
+        burgerTop,
+        burgerBottom
+    ) {
+        this.burgers = burgers;
+        this.lettuce = lettuce;
+        this.tomato = tomato;
+        this.burgerTop = burgerTop;
+        this.burgerBottom = burgerBottom;
+    }
+}
+
+function loadAssets() {
+    let burgerEntities = document.querySelectorAll(BURGER_CLASS);
+    let lettuceEntities = document.querySelectorAll(BURGER_CLASS);
+    let tomatoEntities = document.querySelectorAll(BURGER_CLASS);
+    let burgerTopEntities = document.querySelectorAll(BURGER_CLASS);
+    let burgerBottomEntities = document.querySelectorAll(BURGER_CLASS);
+
+    let order = new Order(
+        burgerEntities,
+        lettuceEntities,
+        tomatoEntities,
+        burgerTopEntities,
+        burgerBottomEntities);
+
+    return {
+        get: () => order,
+        set: (newOrder) => order = newOrder
+    }
+}
+
+window.onload = () => {
+    console.log(loadAssets().get());
 }
 
 function resolveOrder(componentList) {
@@ -11,5 +55,5 @@ function resolveOrder(componentList) {
 }
 
 function checkOrder(orderList) {
-    
+
 }
