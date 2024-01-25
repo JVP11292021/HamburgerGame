869c4e9aa8e9f0e2f6103e8f04fdc1214046d471
albion16 <albionilazi03@gmail.com>
2024-01-25 14:13:37 +0100

    Werkende oppak code (ingrediënten)

--- src/app/controller.js
+++ src/app/controller.js
@@ -6,23 +6,21 @@ let interactedObject;
 function pickup() {
     if (obbCollisionStarted && gripDown && !swBool) {
         // Get the hand element
-        let object = document.getElementById("burgert")
         let hand = document.getElementById("rightHand");
 
         // Get the box element
-        object.setAttribute("visible", "false");
 
         // Clone the box element
-        let clonedObject = object.cloneNode(true);
+        let clonedObject = interactedObject.cloneNode(true);
+        clonedObject.removeAttribute("obb-collider");
+        interactedObject.setAttribute("visible", "false");
 
         // Modify the position attribute of the cloned box
         clonedObject.setAttribute("position", "0 0 0");
         clonedObject.setAttribute("id", "cloneobject");
-        clonedObject.removeAttribute("obb-collision");
 
         // Append the cloned box to the hand element
         hand.appendChild(clonedObject);
-
         swBool = true;
     }
 }
@@ -31,13 +29,12 @@ function release() {
     swBool = false
     let cloneobject = document.getElementById("cloneobject")
     let hand = document.getElementById("rightHand");
-    let box = document.getElementById("box");
     const position = hand.getAttribute("position")
     const rotation = hand.getAttribute("rotation")
     cloneobject.remove();
-    box.setAttribute("position", position)
-    box.setAttribute("rotation", rotation)
-    box.setAttribute("visible", "true");
+    interactedObject.setAttribute("position", position)
+    interactedObject.setAttribute("rotation", rotation)
+    interactedObject.setAttribute("visible", "true");
 }
 
 const main = () => {
@@ -48,6 +45,9 @@ const main = () => {
         const object = objectList[i];
     object.addEventListener("obbcollisionstarted", function () {
         obbCollisionStarted = true;
+        if (!gripDown) {
+           interactedObject = object 
+        }
         pickup();
     });
     object.addEventListener("obbcollisionended", function () {
@@ -57,7 +57,9 @@ const main = () => {
 
     hand.addEventListener("gripdown", function () {
         gripDown = true;
-        pickup();
+        if (interactedObject != null) {
+            pickup();
+        }
     });
 
     // Reset the flags when the corresponding events end
@@ -66,6 +68,7 @@ const main = () => {
     hand.addEventListener("gripup", function () {
         gripDown = false;
         release();
+        interactedObject = null
     });
 };
 
